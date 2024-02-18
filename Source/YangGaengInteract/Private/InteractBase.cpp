8f8f08a06abf5ce04a80dc2102abf0a3cca27421
Freode <free4760@naver.com>
2024-02-18 13:07:58 +0900

    Apply Module Test

--- Source/YangGaengInteract/Private/InteractBase.cpp
+++ Source/YangGaengInteract/Private/InteractBase.cpp
@@ -9,6 +9,18 @@ AInteractBase::AInteractBase()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// ===== 계층 =====
+
+	Scene = CreateDefaultSubobject<USceneComponent>(TEXT("SCENE"));
+	SM_Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SM_MESH"));
+	SK_Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SK_MESH"));
+	Box = CreateDefaultSubobject<UBoxComponent>(TEXT("BOX"));
+
+	RootComponent = Scene;
+	SM_Mesh->SetupAttachment(Scene);
+	SK_Mesh->SetupAttachment(Scene);
+	Box->SetupAttachment(Scene);
+
 }
 
 // Called when the game starts or when spawned
