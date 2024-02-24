15f4098697d1ed7e94105ad9dae5fed3b75852b0
Freode <free4760@naver.com>
2024-02-24 20:48:02 +0900

    플레이어 입력 시스템 구현 1

--- Source/YangGaeng/Public/Player/ContentCreatedPawn.h
+++ Source/YangGaeng/Public/Player/ContentCreatedPawn.h
@@ -2,8 +2,11 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "../../YangGaeng.h"
 #include "GameFramework/Pawn.h"
+#include "InputActionValue.h"
+#include "InputMappingContext.h"
+
 #include "ContentCreatedPawn.generated.h"
 
 UCLASS()
@@ -11,6 +14,7 @@ class YANGGAENG_API AContentCreatedPawn : public APawn
 {
 	GENERATED_BODY()
 
+// =================== FUNCTION ===================
 public:
 	// Sets default values for this pawn's properties
 	AContentCreatedPawn();
@@ -19,11 +23,56 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	// Move Action에 바인딩할 함수
+	void Move(const FInputActionValue& Value);
+
+	// Spin Action에 바인딩할 함수
+	void Spin(const FInputActionValue& Value);
+
+	// Zoom Action에 바인딩할 함수
+	void Zoom(const FInputActionValue& Value);
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+// =================== PROPERTY ===================
+public:
+
+	// 루트 컴포넌트 역할
+	UPROPERTY(VisibleAnywhere, Category = Component)
+		USceneComponent* MainScene;
+
+	// 카메라 길이 조절 컴포넌트
+	UPROPERTY(VisibleAnywhere, Category = Component)
+		USpringArmComponent* CameraBoom;
+
+	// 카메라 컴포넌트
+	UPROPERTY(VisibleAnywhere, Category = Component)
+		UCameraComponent* Camera;
+
+protected:
+	// ===== Input System =====
+	
+	// Default Mapping Context
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = true))
+		TSoftObjectPtr<UInputMappingContext> DefaultMappingContext;
+
+	// Move
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = true))
+		TObjectPtr<class UInputAction> MoveAction;
+
+	// Spin
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = true))
+		TObjectPtr<class UInputAction> SpinAction;
+
+	// Zoom
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, Meta = (AllowPrivateAccess = true))
+		TObjectPtr<class UInputAction> ZoomAction;
+
+	// DragMove
 
 };
